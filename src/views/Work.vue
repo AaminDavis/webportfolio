2748a6bc8f848bb4eb11cf19950d1577d3d3f658
AaminDavis <49926904+AaminDavis@users.noreply.github.com>
2019-09-06 00:01:24 -0400

    removed  assets and updated workpage

--- src/views/Work.vue
+++ src/views/Work.vue
@@ -1,7 +1,6 @@
 <template>
   <section class="work">
     <div class="container project">
-      <!-- Don't use underscores in file names-->
       <h5>Project One</h5>
       <div class="main_proj_cont">
         <img :src="require('@/assets/img/aeScreenshot.png')" alt="Affordable Elegance, LLC Home Page" />
@@ -19,8 +18,10 @@
     <hr />
     <div class="container project">
       <h5>Project Two</h5>
-      <img src="@/assets/img/zipcodefinder-b1559.firebaseapp.com_(iPhone6_7_8Plus).png" alt="Zip Finder Wep App">
-      <p>This is a demo project that I have worked on to create a PWA using Vue.js and Ionic. I worked on this to get a better understanding of service workers, PWAs, and how the Ionic/Vue package works.</p>
+      <div class="main_proj_cont">
+      <img src="@/assets/img/zipcodefinder-b1559.web.app_.png" alt="Zip Finder Wep App">
+      <p>This is a demo project that I have worked on to create a PWA using Vue.js and Ionic. I worked on this to get a better understanding of service workers, PWAs, how the Ionic/Vue package works and firebase hosting.</p>
+      </div>
       <div class="btns">
         <button>
           <a rel="preconnect" href="https://zipcodefinder-b1559.web.app/#/">View Site</a>
@@ -33,8 +34,10 @@
     <hr />
     <div class="container project">
       <h5>Project Three</h5>
-      <!--<img src="@/img/AE_screenshot.PNG" alt="">-->
+      <div class="main_proj_cont">
+      <img src="@/assets/img/frosty-elion-9792b8.netlify.com_ (2).png" alt="Moses Photography">
       <p>I created this responsive web site for Moses Photography. Moses Photography is a photography studio that was created in response to overly photoshopped and edited photographs, especially of women and to demonstrate the beauty of ordinary items. To keep in line with Moses Photography, I decided to go with a minimalist design, one that emphasizes the content and its elegance.</p>
+      </div>
       <div class="btns">
         <button>
           <a rel="preconnect" href="#">View Site</a>
@@ -47,8 +50,10 @@
     <hr />
     <div class="container project">
       <h5>Project Four</h5>
-      <!--<img src="@/img/AE_screenshot.PNG" alt="">-->
+      <div class="main_proj_cont">
+      <img src="@/assets/img/localhost_8080_.png" alt="Aamin Withrow-Davis Portfolio">
       <p>Coming Soon</p>
+      </div>
       <div class="btns">
         <button>
           <a rel="precommect" href="#">View Site</a>
@@ -58,10 +63,10 @@
         </button>
       </div>
     </div>
-    <hr />
+    <!--<hr />
     <div class="container project">
       <h5>Project Five</h5>
-      <!--<img src="@/img/AE_screenshot.PNG" alt="">-->
+      <img src="@/img/AE_screenshot.PNG" alt="">
       <p>Coming Soon</p>
       <div class="btns">
         <button>
@@ -71,7 +76,7 @@
           <a rel="preconnect" href="#">View Code</a>
         </button>
       </div>
-    </div>
+    </div>-->
   </section>
 </template>
 
